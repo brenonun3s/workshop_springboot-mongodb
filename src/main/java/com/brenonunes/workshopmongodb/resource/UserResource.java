815c989f1b6960f89a9380f2fdc654e5f1cbad65
Breno Nunes <morais.brenonunes@hotmail.com>
2025-01-16 14:49:17 -0300

    Inserido User e UserResource -> Ambos funcionando

---
+++ src/main/java/com/brenonunes/workshopmongodb/resource/UserResource.java
@@ -0,0 +1,30 @@
+package com.brenonunes.workshopmongodb.resource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brenonunes.workshopmongodb.domain.User;
+
+//Para definir que a classe será um recurso REST, usa o @RestController
+//Para definir qual o caminho do EndPoint, usa o RequestMapping
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "Maria Brown", "Maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+
+}
