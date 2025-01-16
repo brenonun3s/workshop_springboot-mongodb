dba69fb670ef82210a8c6367e81046c565068eba
Breno Nunes <morais.brenonunes@hotmail.com>
2025-01-16 17:35:02 -0300

    Adicionando Usuario com Post

--- src/main/java/com/brenonunes/workshopmongodb/services/UserService.java
+++ src/main/java/com/brenonunes/workshopmongodb/services/UserService.java
@@ -7,23 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brenonunes.workshopmongodb.domain.User;
+import com.brenonunes.workshopmongodb.dto.UserDTO;
 import com.brenonunes.workshopmongodb.repository.UserRepository;
 import com.brenonunes.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
